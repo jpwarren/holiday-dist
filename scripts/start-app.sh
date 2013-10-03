243902357d3aaa387c4f58c0ae095bd690246b02
Mark Pesce <mpesce@gmail.com>
2013-10-03 11:10:05 +1000

    Changes for apps to fire off from IoTAS

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #
-# Start the app described by the path in $1 $2 $3 $4
+# Start the app described by the path in $1 $2 $3 $4...
 # Run it in the background, and create a PID file for it
 # Then exit
 #
-$1 $2 $3 $4 >> /dev/null &
+echo "Starting $1 $2 $3 $4 $5 $6 $7 $8 $9"
+#
+$1 $2 $3 $4 $5 $6 $7 $8 $9 >> /dev/null &
 echo $! > /run/iotas-app.pid
 exit 0
